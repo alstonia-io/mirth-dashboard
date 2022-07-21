010a2dac6ede37a6f85a7b29c4bf17aef489ce55
ujjwal1070 <ujjwal1070@gmail.com>
2022-07-21 10:03:48 +0545

    compare two timestamps

--- index.js
+++ index.js
@@ -2,43 +2,41 @@ const express = require('express')
 const DB_CONNECTION = require('./connection')
 const mirth_api = require('./mirth-api')
 const {CREATE_SQL, INSERT_SQL} = require('./schema')
-const {SELECT_ALL} = require('./queries')
+const {SELECT_TIMESTAMPS} = require('./queries')
 
 let sql
-
 const app = express()
 const PORT = 5000
 let token = ''
 
-//query existing stats from table
-let existing_stats = []
-//fetcehd_stats
+//newly fetched stats from mirth client apis
 let fetched_stats = []
+//channel groups
+let channel_groups
 
 
 //create table
 sql = CREATE_SQL
 DB_CONNECTION.run(sql)
 
-
-console.log('existing stats:')
-sql = SELECT_ALL
-DB_CONNECTION.all(sql, [], (err,rows)=>{
-    if(err) return console.log(err.message)
-    existing_stats = [...rows]
-    existing_stats.forEach(stat => {
-        console.log(stat)
-    })
-})
-
 //register view engine
 app.set('view engine','ejs')
-
 //read form data sent from html form element
 app.use(express.urlencoded({extended: true}))
-
 // listen to requests
 app.listen(PORT)
+
+//sqlite3 does not support async/await
+//create an async function with a promise
+async function db_all(query){
+    return new Promise(function(resolve,reject){
+        DB_CONNECTION.all(query, function(err,rows){
+        if(err){return reject(err);}
+        resolve(rows);
+        });
+    });
+}
+
 
 app.get('/',(req,res)=> {
     if(token==''){
@@ -58,29 +56,8 @@ app.get('/channels',(req,res)=>{
         .then(stats=>{
             //copy fetched stats
             fetched_stats = [...stats.channels]
-
-            //delete existing stats from table
-            // console.log('deleting existing stats.')
-            // DB_CONNECTION.run(`DELETE FROM stats`,[],err => {
-            //     if(err) return console.log(err.message)
-            // })
-
-            //insert new stats into table
-            // stats.channels.forEach(channel => {
-            //     sql = `INSERT INTO stats(channelId, channelName, received, sent, error, filtered) VALUES (?,?,?,?,?,?)`
-            //     DB_CONNECTION.run(sql, [channel.channelId, channel.channelName, channel.received, channel.sent, channel.error, channel.filtered], err => {
-            //         if(err) return console.log(err.message)
-            //     })
-            // })
-            //newly stored stats
-            // console.log('newly stored stats:')
-            // DB_CONNECTION.all(`SELECT * FROM stats`,[],(err, rows)=>{
-            //     if(err) return console.log(err.message)
-            //     rows.forEach(row => {
-            //         console.log(row)
-            //     })
-            // })
-            res.render('channels',{channels : fetched_stats, channelGroups : stats.channelGroups})
+            channel_groups = [...stats.channelGroups]
+            res.render('channels',{channels : fetched_stats, channelGroups : channel_groups})
         })
         .catch(error => res.send("Cannot fetch channels!! Service temporarily unavailable."))
     }
@@ -122,4 +99,62 @@ app.post('/',(req,res)=>{
             res.redirect('/channels')
         })
         .catch(error => res.render('index',{msg : 'Incorrect credentials' })  )
+})
+
+app.get('/timestamps',(req,res)=> {
+    if(token==''){
+        res.render('index', {msg : null})
+    }
+    else{
+        sql = SELECT_TIMESTAMPS
+        DB_CONNECTION.all(sql,[],(err,rows)=>{
+            if(err) console.log(err.message)
+            res.render('timestamps',{stats: rows})
+        })
+    }
+})
+
+app.post('/timestamps',async (req,res)=> {
+    //list of timestamps that were selected 
+    let selected_timestamps = []
+    let recent_timestamp
+    let older_timestamp
+
+    //recent stats
+    let recent_stats = []
+    ///older stats
+    let older_stats = []
+
+    for(checkbox in req.body){
+        selected_timestamps.push(checkbox)
+    }
+    //make sure two timestamps are selected
+    if(selected_timestamps.length != 2){
+        res.send('Please select two timestamps to compare.')
+    }
+    else{
+        //sort the timestamps
+        selected_timestamps.sort(function(a, b){return a - b})
+        recent_timestamp = selected_timestamps[1] 
+        older_timestamp = selected_timestamps[0]
+
+        //fetch recent stats
+        sql = `SELECT * FROM stats WHERE timestamp = ${recent_timestamp}`
+        recent_stats = await db_all(sql)
+
+        //fetch older stats
+        sql = `SELECT * FROM stats WHERE timestamp = ${older_timestamp}`
+        older_stats = await db_all(sql)
+
+        //generate compared results
+        //overwrite compared results to recent_stats
+        recent_stats.forEach(stat => {
+            older_stat = older_stats.find(older_stat => older_stat.channelId == stat.channelId )
+            stat.received   = stat.received - older_stat.received
+            stat.sent       = stat.sent - older_stat.sent
+            stat.error      = stat.error - older_stat.error
+            stat.filtered   = stat.filtered - older_stat.filtered
+        })
+        res.render('channels',{channels : recent_stats, channelGroups : channel_groups})
+    }
 })
--- queries.js
+++ queries.js
@@ -1,5 +1,7 @@
 const SELECT_ALL = 'SELECT * FROM stats'
+const SELECT_TIMESTAMPS = 'SELECT DISTINCT timestamp FROM stats ORDER BY timestamp DESC'
 
 module.exports = {
-    SELECT_ALL
+    SELECT_ALL,
+    SELECT_TIMESTAMPS
 }
